6b1de316845ff0d103780445ce4e4e2bbf7d012e
Darien Bernal <darien.bernal@gmail.com>
2025-01-24 22:51:27 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Part 1: Thinking Functionally
+const numbers = [3, 7, 12, 5, 20, 8, 15];
+
+// Take an array of numbers and return the sum.
+
+
+// Take an array of numbers and return the average.
+// Take an array of strings and return the longest string.
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
