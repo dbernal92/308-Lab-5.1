c0ac881c86a801252947d4fc6b287171e189b588
Darien Bernal <darien.bernal@gmail.com>
2025-01-25 01:58:52 -0500

    Fixed strNum function to iterate through array

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ console.log(arraySum(numbers))
 
 // Take an array of numbers and return the average.
 function arrayAvg(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
+    let sum = arraySum(arr);
     return sum / arr.length;
 }
 
@@ -38,5 +35,18 @@ function longestStr(arr) {
 
 console.log("Longest string:", longestStr(strings))
 
-
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+let n = 4;
+
+function strNum(arr) {
+    
+    let holder = new Array ();
+    arr.forEach(value => {
+        if (value.length > n) {
+            holder.push(value);
+        }
+    })
+    return holder;
+}
+
+console.log(strNum(strings))
