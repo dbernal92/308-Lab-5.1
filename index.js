6c5e770a5d56f730e9dcf98f591f33597978ba4d
Darien Bernal <darien.bernal@gmail.com>
2025-01-24 23:37:05 -0500

    Created arraySum and arrayAvg

--- index.js
+++ index.js
@@ -2,8 +2,34 @@
 const numbers = [3, 7, 12, 5, 20, 8, 15];
 
 // Take an array of numbers and return the sum.
+function arraySum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
 
+console.log(arraySum(numbers))
 
 // Take an array of numbers and return the average.
+function arrayAvg(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(arrayAvg(numbers))
+
 // Take an array of strings and return the longest string.
+const strings = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"];
+
+function longestStr(arr) {
+    for (i = 0; i < arr.length; i++) {
+        
+    }
+}
+
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
